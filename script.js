97804f884c10fb827ed6d722de0e1e95bbf3fab1
Dominik Pyzia <62100358+DPyzia@users.noreply.github.com>
2024-10-10 08:59:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+let descriptionShowing = false;
+
 const squares = document.querySelectorAll('.square');
 
 squares.forEach((square) => {
   square.addEventListener('click', () => {
+    if (descriptionShowing) return; // prevent clicking on other squares
+
     const description = square.nextElementSibling;
     description.classList.toggle('show');
+    descriptionShowing = !descriptionShowing; // toggle the flag
   });
 });
