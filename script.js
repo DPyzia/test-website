656e1e76e05ec381462a9893ac3a981209e8e48d
Dominik Pyzia <62100358+DPyzia@users.noreply.github.com>
2024-10-11 11:40:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+let descriptionShowing = false;
+
 const squares = document.querySelectorAll('.square');
 const descriptions = document.querySelectorAll('.description');
 
 squares.forEach(function (el) {
   el.addEventListener('click', function (e) {
+    if (descriptionShowing) return
+
     el.nextElementSibling.style.display = 'block';
+    descriptionShowing = !descriptionShowing;
   })
 });
 
 descriptions.forEach(function (el) {
   el.addEventListener('click', function (e) {
     el.style.display = 'none';
+
+    descriptionShowing = false;
   })
 });
