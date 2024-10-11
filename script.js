62f6181fa4f78508ff7302c8440382ba38f32af3
Dominik Pyzia <62100358+DPyzia@users.noreply.github.com>
2024-10-11 11:16:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const togglers = document.querySelectorAll('.square');
-togglers.forEach(function(el) {
-  el.addEventListener('click', function(e) {
-    el.nextElementSibling.style.display = 'block';
-  })
+const squareContainer = document.querySelector('.square-container');
+
+squareContainer.addEventListener('click', function(e) {
+  if (e.target.classList.contains('square')) {
+    const description = e.target.nextElementSibling;
+    description.style.display = description.style.display === 'block' ? 'none' : 'block';
+  } else if (e.target.classList.contains('description')) {
+    e.target.style.display = 'none';
+  }
 });
